65481c175c74b589842927a0e6d09f2d8bd57825
PutuRifkiDy <puturifki56@gmail.com>
2025-11-11 08:49:34 +0800

    added: setup configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
+
+export default [
+  daStyle,
+  pluginJs.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs', globals: globals.node },
+  },
+];
