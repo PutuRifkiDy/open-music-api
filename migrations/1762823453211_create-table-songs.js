612c4420552c2390de4b368264f43f99d2433e22
PutuRifkiDy <puturifki56@gmail.com>
2025-11-11 09:31:03 +0800

    added: migration table songs

---
+++ migrations/1762823453211_create-table-songs.js
@@ -0,0 +1,54 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+      notNull: false,
+    },
+    albumId: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+    },
+    createdAt: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updatedAt: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
