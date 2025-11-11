a0a0a30f2f832f4771d205034f2775ffe30ac222
PutuRifkiDy <puturifki56@gmail.com>
2025-11-11 08:51:37 +0800

    added: migration

---
+++ migrations/1762800381511_create-table-albums.js
@@ -0,0 +1,38 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    createdAt: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updatedAt: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};
