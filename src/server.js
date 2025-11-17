cd99621324ad3f2954eec889926503675eefa39f
PutuRifkiDy <puturifki56@gmail.com>
2025-11-17 15:41:13 +0800

    added: register plugins in server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const AlbumValidator = require('./validator/albums');
+const songs = require('./api/songs');
+const SongsServices = require('./services/postgres/SongsService');
+const SongValidator = require('./validator/songs');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsServices();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -10,6 +20,38 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+        validator: AlbumValidator,
+      }
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongValidator,
+      }
+    }
+  ]);
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
