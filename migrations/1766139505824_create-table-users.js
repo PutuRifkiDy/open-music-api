f2585b3cd6f29489e0e344e1f57b7e3023aa6e94
PutuRifkiDy <puturifki56@gmail.com>
2025-12-19 18:49:51 +0800

    added: addUser and verifyNewUsername function in UsersService

--- migrations/1766139505824_create-table-users.js
+++ migrations/1766139505824_create-table-users.js
@@ -1,3 +1,26 @@
-export const up = (pgm) => {};
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
 
-export const down = (pgm) => {};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
