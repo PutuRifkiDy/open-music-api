e9e4b307998c935481cfabc8b5ee24151b55488e
PutuRifkiDy <puturifki56@gmail.com>
2025-12-21 15:24:15 +0800

    added: addPlaylistSongActivity function in PlaylistSongsService

--- src/services/postgres/PlaylistSongsService.js
+++ src/services/postgres/PlaylistSongsService.js
@@ -21,6 +21,24 @@ class PlaylistSongsService {
     }
   }
 
+  async addPlaylistSongActivity(playlistId, songId, userId, action) {
+    const id = `playlistactivity-${nanoid(16)}`;
+    const time = new Date().toISOString();
+
+    const query = {
+      text: 'INSERT INTO playlist_song_activities VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, playlistId, songId, userId, action, time],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Aktivitas lagu pada playlist gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
   async addSongToPlaylist(playlistId, songId) {
     const id = `playlistsong-${nanoid(16)}`;
     const createdAt = new Date().toISOString();
